0ae1b0282fda60853c1dbb7b9d8d905050bdf75f
shreya-1306 <33290115+shreya-1306@users.noreply.github.com>
2019-06-25 14:52:05 +0530

    Add files via upload

--- admin_index.php
+++ admin_index.php
@@ -18,6 +18,8 @@ Licence URI: http://www.os-templates.com/template-terms
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" type="text/css" href="faq.css"> 
+  <script src="faq.js"></script>
 </head>
 <body id="top">
 <!-- ################################################################################################ -->
@@ -34,7 +36,7 @@ Licence URI: http://www.os-templates.com/template-terms
       </div>
       <nav id="mainav" class="fl_right">
         <ul class="clear">
-          <li class="active"><a href="home.php">Home</a></li>
+          <li class="active"><a href="admin_index.php">Home</a></li>
           <li><a class="drop" href="#">Search By</a>
             <ul>
               <li><a href="searchby/rollno.php">Roll No.</a></li>
@@ -61,7 +63,7 @@ Licence URI: http://www.os-templates.com/template-terms
             <ul>
              
               <li><a href="feedback_a.php">Contact Us</a></li>
-              <li><a href="contact_after_admin.php">Help</a></li>
+              <li><a href="admin_index.php">Help</a></li>
               <li><a href="changepass_a.php">Change Password</a></li>
               <li><a href="logout.php">Logout</a></li>
             </ul>
@@ -78,35 +80,112 @@ Licence URI: http://www.os-templates.com/template-terms
   <main class="hoc container clear"> 
     <!-- main body -->
     <!-- ################################################################################################ -->
-    <div class="sectiontitle">
-      <h6 class="heading">PERSONAL DETAILS</h6>
-      
-    </div>
-    <div class="group excerpts">
-   
-      <article style="margin-left: 342px;" class="one_third">
-        <div class="hgroup">
-          <h6 class="heading">  <?php if(isset($_SESSION['username'])){ $usersData = getFacultyData(getFacultyID($_SESSION['username']));echo $usersData['Name']; }
-?>
-          </h6>
-          <!-- <em>General Manager</em></div> -->
+  
+    
 
-        
-        <div class="txtwrap">
-          <p> ID NO: <?php 
-  if(isset($_SESSION['username'])){ $usersData = getFacultyData(getFacultyID($_SESSION['username'])); echo $usersData['ID'];}
-?>
-</p>  <p>
-Type
-<?php if(isset($_SESSION['username'])){ $usersData = getFacultyData(getFacultyID($_SESSION['username'])); echo $usersData['Type']; }
-?>
+    <!--  FAQs START -->
 
-          </p>
-        </div>
-      
-      </article>
+    <fieldset>
+
+    <div class="contact">
+  
+
+    <dl class="faq">
+        <h3>INSTRUCTIONS: </h3>
+  <dt>
+    <button aria-expanded="false" aria-controls="faq1_desc">
+      1.  How do I login for the first time?
+    </button>
+  </dt>
+  <dd>
+    <p id="faq1_desc" class="desc">
+      Ans : Click on First Time User/Forgot password and enter your Somaiya ID to get your initial password on your mail-id.After logging in you can 
+      change your password for your account.
+  </dd>
+  <dt>
+    <button aria-expanded="false" aria-controls="faq2_desc">
+      2. Can I view my progress or the activities done by me ?
+    </button>
+  </dt>
+  <dd>
+    <p id="faq2_desc" class="desc">
+      Ans : Yes,you can view your progress on clicking on VIEW MY PROGRESS button which will show your uploaded activities and 
+      details under various categories.You can also delete an entry in case you dont want the entry to be there.
+    </p>
+  </dd>
+  <dt>
+    <button aria-expanded="false" aria-controls="faq3_desc">
+      3. How do I add an activity?
+    </button>
+  </dt>
+  <dd>
+    <p id="faq3_desc" class="desc">
+     Ans : You can simply add an activity by clicking on ADD NEW ACTIVITY which will show various categories of activities
+     you can upload.Just click on any category,add details accordingly and press submit.
      
-    </div>
+    </p>
+  </dd>
+  <dt>
+    <button aria-expanded="false" aria-controls="faq4_desc">
+      4. Do I need a proof for the  activity I upload?
+    </button>
+  </dt>
+  <dd>
+    <p id="faq4_desc" class="desc">
+    Ans : Yes definitely. You should upload some proof like a certificate or a Letter of Appreciation pertaining to the activity you upload.
+    It should be in doc or pdf format.Additional image can also be uploaded(if any).
+    </p>
+  </dd>
+  <dt>
+    <button aria-expanded="false" aria-controls="faq5_desc">
+      5. Is there any rule for the name of the files to be uploaded?
+    </button>
+  </dt>
+  <dd>
+    <p id="faq5_desc" class="desc">
+     Ans : For uniquely attributing each file to a user,the name of the file needs to be [SerialNumber_YourRollnumber].
+     More details regarding name of the files is provided in the add new activity bar while uploading the files.
+     
+    </p>
+  </dd>
+  <dt>
+  <button aria-expanded="false" aria-controls="faq6_desc">
+      6. What if I need to see my classmates uplaods?
+    </button>
+  </dt>
+   <dd>
+    <p id="faq6_desc" class="desc">
+     Ans : There is a menu of the left side of the page with the title "View". It displays various entries done by different users.
+    </p>
+  </dd>
+  <dt>
+    <button aria-expanded="false" aria-controls="faq7_desc">
+     7. Want to report a bug?
+    </button>
+  </dt>
+  <dd>
+    <p id="faq7_desc" class="desc">
+     Ans :  Shreya Varma    : shreya.varma@somaiya.edu <br>
+      Aromal Nair     : aromal.nair@somaiya.edu <br>
+      Priyanka Kalena : priyanka.kalena@somaiya.edu <br>
+      
+      Feel free to email us for feedbacks :)
+    </p>
+  <p style="    font-size: 19px;
+    margin-left: 681px;
+    margin-top: 157px;">  Have any other queries? Ask us at : someid@somaiya.edu</p>
+  </dd>
+
+</dl>
+
+
+
+
+
+</div>
+
+<fieldset>
+    <!--  FAQs END -->
     <!-- ################################################################################################ -->
     <!-- / main body -->
     
