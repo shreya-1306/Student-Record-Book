276cf7a4b630e54c5893ec04e6baa2b3c0cb9318
asn_97 <30555819+ASN97@users.noreply.github.com>
2019-07-24 17:58:44 +0530

    Add files via upload

--- php/admin_view/view_tpp.php
+++ php/admin_view/view_tpp.php
@@ -23,6 +23,20 @@ Licence URI: http://www.os-templates.com/template-terms
     $('#workshop').DataTable();
 } );
 </script>
+ <style>
+footer{
+
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: black;
+   color: white;
+   #A2B70D
+   text-align: center;
+   height:max-content;
+}
+</style>
 </head>
 <body id="top">
 <!-- ################################################################################################ -->
@@ -65,15 +79,18 @@ Licence URI: http://www.os-templates.com/template-terms
               <li><a href="../../graphical.php">Graphical</a></li>
             </ul>
           </li>
-          <li><a class="drop" href="#">Other</a>
+         <li><a class="drop" href="#">Other</a>
             <ul>
              
-              <li><a href="../../feedback_a.php">Contact Us</a></li>
-              <li><a href="../../contact_after_admin.php">Help</a></li>
+             
+              
+             
+              <li><a href="../../admin_index.php">Help</a></li>
               <li><a href="../../changepass_a.php">Change Password</a></li>
               <li><a href="../../logout.php">Logout</a></li>
             </ul>
           </li>
+         
          
      
         </ul>
@@ -200,7 +217,13 @@ $conn->close();
     
   </main>
 </div>
+<center>
+ <footer>
+<h3 style="margin-top:5px;"> For any further queries Email us at admin@somaiya.edu</h3>
+<a href="feedback_a.php" style="font-size: 20px;margin-top:-5px;">Click here for feedback / queries </a>
+</footer>
 
+</center>
 </div><!--  background image tag -->
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
