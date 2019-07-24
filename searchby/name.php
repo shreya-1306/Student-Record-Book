35862212c701e6c58eec572776553ce0d653ab6d
asn_97 <30555819+ASN97@users.noreply.github.com>
2019-07-24 17:59:34 +0530

    Add files via upload

--- searchby/name.php
+++ searchby/name.php
@@ -18,7 +18,27 @@ Licence URI: http://www.os-templates.com/template-terms
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link href="../layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+<style>
 
+
+
+footer{
+
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: black;
+   color: white;
+   #A2B70D
+   text-align: center;
+   height:max-content;
+}
+
+
+
+
+</style>
 </head>
 <body id="top">
 <!-- ################################################################################################ -->
@@ -60,15 +80,16 @@ Licence URI: http://www.os-templates.com/template-terms
               <li><a href="../graphical.php">Graphical</a></li>
             </ul>
           </li>
-          <li><a class="drop" href="#">Other</a>
+         <li><a class="drop" href="#">Other</a>
             <ul>
              
-              <li><a href="../feedback_a.php">Contact Us</a></li>
-              <li><a href="../admin_index.php">Help</a></li>
-              <li><a href="../changepass_a.php">Change Password</a></li>
-              <li><a href="../logout.php">Logout</a></li>
+             
+              <li><a href="admin_index.php">Help</a></li>
+              <li><a href="changepass_a.php">Change Password</a></li>
+              <li><a href="logout.php">Logout</a></li>
             </ul>
           </li>
+         
          
      
         </ul>
@@ -593,7 +614,13 @@ $conn->close();
     
   </main>
 </div>
+<center>
+ <footer>
+<h3 style="margin-top:5px;"> For any further queries Email us at admin@somaiya.edu</h3>
+<a href="feedback_a.php" style="font-size: 20px;margin-top:-5px;">Click here for feedback / queries </a>
+</footer>
 
+</center>
 </div><!--  background image tag -->
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
